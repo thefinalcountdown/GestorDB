ce3a34fe57fdcbfa975e2920e7520df0b8fb5b65
Ledesin <46573074+Ledesin@users.noreply.github.com>
2019-04-03 15:53:16 +0200

    Add files via upload

---
+++ GestorDB/src/main/java/main/mainBD.java
@@ -0,0 +1,13 @@
+package main;
+
+import metodos.GestorBD;
+
+public class mainBD {
+
+	public static void main(String[] args) {
+		GestorBD gestorBD = new GestorBD();
+
+		gestorBD.getConexion();
+	}
+
+}
---
+++ GestorDB/src/main/java/metodos/GestorBD.java
@@ -0,0 +1,41 @@
+package metodos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GestorBD {
+	private String maquina = "localhost";
+	private String usuario = "root";
+	private String clave = "elorrieta";
+	private int puerto = 3306;
+	private String servidor = "";
+	private static Connection conexion = null;
+
+	public GestorBD() {
+
+		servidor = "jdbc:mysql://" + maquina + ":" + puerto + "/bidaion?serverTimezone=UTC";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Error al registrar el Driver");
+			System.exit(0);
+		}
+
+		try {
+			conexion = DriverManager.getConnection(servidor, usuario, clave);
+		} catch (SQLException e) {
+			System.err.println("Error al conectar con el servidor");
+			System.exit(0);
+		}
+		System.out.println("Conectando a la base de datos...");
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+}
