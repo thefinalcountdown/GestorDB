f47159c6badfbb3f746e126ebba907d97c840a76
Pivorruchi <ivorrairis@gmail.com>
2019-05-11 19:33:01 +0200

    cambiar sentencia ubicacion

--- GestorDB/src/main/java/metodos/GestorBD.java
+++ GestorDB/src/main/java/metodos/GestorBD.java
@@ -29,166 +29,113 @@ public class GestorBD {
 	public GestorBD() {
 
 		servidor = "jdbc:mysql://" + maquina + ":" + puerto + "/bidaion_prueba?serverTimezone=UTC";
-		
-		try 
-		{
+
+		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-		} 
-		catch (ClassNotFoundException e) 
-		{
+		} catch (ClassNotFoundException e) {
 			JOptionPane.showMessageDialog(null, "Error al registrar el Driver.");
 			// System.err.println("Error al registrar el Driver");
 			System.exit(0);
 		}
 
-		try 
-		{
+		try {
 			conexion = DriverManager.getConnection(servidor, usuario, clave);
-		} 
-		catch (SQLException e) 
-		{
+		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "Error al conectar con el servidor.");
 			// System.err.println("Error al conectar con el servidor");
 			System.exit(0);
 		}
-		
+
 		conectando();
-		
+
 	}
 
 	public static JOptionPane conectando() {
 		JOptionPane miPanel = new JOptionPane("Conectando a la Base de datos.");
-        final JDialog dialog = miPanel.createDialog(null, "Conectando");//aqui uso un JDialog para manipular
-        																					//el JOptionPane
-        dialog.setModal(true);  
-        //Usando javax.swing.Timer para poder generar un evento en un tiempo determinado
-        Timer timer = new Timer(1 * 1000, new ActionListener() {
+		final JDialog dialog = miPanel.createDialog(null, "Conectando");// aqui uso un JDialog para manipular
+																		// el JOptionPane
+		dialog.setModal(true);
+		// Usando javax.swing.Timer para poder generar un evento en un tiempo
+		// determinado
+		Timer timer = new Timer(1 * 1000, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				dialog.dispose();				
-			}		
-        });
-        		
-        timer.start();
-        dialog.setVisible(true);
-        timer.stop();
-        
-        return miPanel;
+				dialog.dispose();
+			}
+		});
+
+		timer.start();
+		dialog.setVisible(true);
+		timer.stop();
+
+		return miPanel;
 	}
-	
-	public static Connection getConexion() 
-	{
+
+	public static Connection getConexion() {
 		return conexion;
 	}
 
-	public static ArrayList<String> obtenerUbicaciones() throws Exception 
-	{
+	public static ArrayList<String> obtenerUbicaciones() throws Exception {
 		ArrayList<String> ubicaciones = new ArrayList<String>();
-		String sentencia = "select distinct(ubicacion) from hoteles order by ubicacion";
-		try 
-		{
+		String sentencia = "select distinct(ubicacion) from alojamiento order by ubicacion";
+		try {
 
 			statement = conexion.createStatement();
 
 			result = statement.executeQuery(sentencia);
-			while (result.next()) 
-			{
+			while (result.next()) {
 				ubicaciones.add(new String(result.getString("ubicacion")));
 
 			}
 
-		} 
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 		return ubicaciones;
 	}
 
-	public static ArrayList<String> obtenerHoteles(String ubicacion) throws Exception 
-	{
-		ArrayList<String> hoteles = new ArrayList<String>();
-		String sentencia = "select * from hoteles where ubicacion='%s'";
-		sentencia = String.format(sentencia, ubicacion);
-		try 
-		{
+	public static ResultSet consulta(String sentencia) {
 
-			statement = conexion.createStatement();
-
-			result = statement.executeQuery(sentencia);
-			while (result.next()) 
-			{
-				String nombre = result.getString("nombre");
-				String precio = Integer.toString(result.getInt("precio"));
-				String estrellas = Integer.toString(result.getInt("estrellas"));
-				hoteles.add(new String(nombre + ";" + precio + "; " + estrellas));
-
-			}
-
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			System.exit(0);
-		}
-		return hoteles;
-	}
-
-	public static ResultSet consulta(String sentencia) 
-	{
-
-		try 
-		{
+		try {
 			preparedstatement = conexion.prepareStatement(sentencia);
 			result = preparedstatement.executeQuery();
-		}
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "No se pudo hacer la consulta a la base de datos.");
 			System.exit(0);
 		}
 		return result;
 	}
 
-	public static boolean insertarDatos(String sentencia) 
-	{
-		try 
-		{
+	public static boolean insertarDatos(String sentencia) {
+		try {
 			statement = conexion.createStatement();
 			statement.executeUpdate(sentencia);
-		}
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "No se pudo insertar los datos.");
 			System.exit(0);
 			return false;
 		}
 		return true;
 	}
-	
-	public static String conseguir_nombre_apellidos(String DNI) 
-	{
-	    CallableStatement call_procedure = null;
-	    String nomape = "";
-	    try 
-	    {
-	          call_procedure = conexion.prepareCall("{call conseguir_nombre_apellidos(?)}");
-	          call_procedure.setString(1,DNI);
-	          
-		      result = call_procedure.executeQuery();
-	
-		      while (result.next()) 
-		      {
-		    	  nomape = result.getString("NomApe");
-		      }
-	    } 
-	    catch (SQLException e) 
-	    {
-	    	JOptionPane.showMessageDialog(null, "No se pudo hacer la consulta a la base de datos.");
-	    	System.exit(0);
-	    } 
-	    
-	    return nomape;
-	 }	
+
+	public static String conseguir_nombre_apellidos(String DNI) {
+		CallableStatement call_procedure = null;
+		String nomape = "";
+		try {
+			call_procedure = conexion.prepareCall("{call conseguir_nombre_apellidos(?)}");
+			call_procedure.setString(1, DNI);
+
+			result = call_procedure.executeQuery();
+
+			while (result.next()) {
+				nomape = result.getString("NomApe");
+			}
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "No se pudo hacer la consulta a la base de datos.");
+			System.exit(0);
+		}
+
+		return nomape;
+	}
 }
